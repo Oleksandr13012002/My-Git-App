18e2a5b52552448c50dbb8ad3f9aee5221e22dbe
Oleksandr13012002 <guzoleksandr37@gmail.com>
2025-02-28 10:54:58 +0200

    Fix app description

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,7 +3,7 @@ package app;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("y git app for Users.");
-        System.out.println("The informative application.");
+        System.out.println("My git app for Users!");
+        System.out.println("The informative application!");
     }
 }
